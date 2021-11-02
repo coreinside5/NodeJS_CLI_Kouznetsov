121f7dcf4f1a5cae9b69b67658ee565507699034
Maxim K <90226452+coreinside5@users.noreply.github.com>
2021-11-02 11:26:19 +0100

    noerrtest

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 #!/usr/bin/env node;
-//console.log("Hello, Node.JS!");
-
-//console.log(process.argv);
-
 import fetch from 'node-fetch';
+import { getCode, getName } from 'country-list';
+import chalk from 'chalk';
+import readlineSync from 'readline-sync';
+ 
 
-   // console.log(process.argv);
-   const website = process.argv[2]; 
 
-   function CheckWeb(name) {
-        const info =fetch(`https://isitup.org/${name}.json`)
-        .then(response => response.json());
+console.clear();
+var year = readlineSync.question("Enter the year :");
+do {
+    console.clear();
+    console.log("Year is " + year);
+   var countryName = getCode(readlineSync.question("Enter country name :"));
+    console.log(countryName); 
+} while (countryName == undefined);
 
-        info.then(function(result) {
-            if (result.response_code == 200) {
-                console.log('website is up and running')
-            } else {
-               console.log('website is down')
-            }
-        });
+
+
+
+   async function GetFest(paramYear, paramCN){
+       let fest = await fetch (`https://date.nager.at/api/v3/publicholidays/${paramYear}/${paramCN}`)
+       .catch(error =>{console.error("There were an error")});
+       let result = await fest.json()
+       console.clear()
+       console.log(chalk.black.bgGreen(`List of holidays in ${getName(paramCN)} for the year ${paramYear}`))
+       for (let index = 0; index < result.length; index++){
+           console.log(chalk.blue(result[index].date) + " " + chalk.cyan(result[index].localName) + "\t " + chalk.red(result[index].name));
+       }
+       
+     
    }
-
-   CheckWeb(website);
+   GetFest(year,countryName);
