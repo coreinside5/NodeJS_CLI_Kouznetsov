85cf31acca4a433a56584d12f8d4e0785f28947e
Maxim K <90226452+coreinside5@users.noreply.github.com>
2021-11-02 13:39:19 +0100

    first final

--- index.js
+++ index.js
@@ -3,31 +3,39 @@ import fetch from 'node-fetch';
 import { getCode, getName } from 'country-list';
 import chalk from 'chalk';
 import readlineSync from 'readline-sync';
- 
+//import figlet from 'figlet';
+//import ora from 'ora';
 
 
-console.clear();
-var year = readlineSync.question("Enter the year :");
-do {
+
+
+console.clear();  
+var year = parseInt(readlineSync.question(chalk.yellow(`Enter the year :`)));
+while (year > 2030 || year < 1700 || isNaN(year)==true){
+    console.clear();   
+    year = readlineSync.question(chalk.yellow(`Bad input, reenter the year :`));     
+} 
+var countryName = getCode(readlineSync.question(chalk.yellow("Enter country name :")));   
+while (countryName == undefined){
     console.clear();
-    console.log("Year is " + year);
-   var countryName = getCode(readlineSync.question("Enter country name :"));
-    console.log(countryName); 
-} while (countryName == undefined);
-
-
+    console.log(chalk.yellow(`Year is  ${year}`));
+    countryName = getCode(readlineSync.question(chalk.yellow("Bad input, reenter country name :")));   
+} 
 
 
    async function GetFest(paramYear, paramCN){
-       let fest = await fetch (`https://date.nager.at/api/v3/publicholidays/${paramYear}/${paramCN}`)
-       .catch(error =>{console.error("There were an error")});
+       try{
+            let fest = await fetch (`https://date.nager.at/api/v3/publicholidays/${paramYear}/${paramCN}`)
+       
        let result = await fest.json()
        console.clear()
        console.log(chalk.black.bgGreen(`List of holidays in ${getName(paramCN)} for the year ${paramYear}`))
        for (let index = 0; index < result.length; index++){
            console.log(chalk.blue(result[index].date) + " " + chalk.cyan(result[index].localName) + "\t " + chalk.red(result[index].name));
        }
-       
-     
+       }catch(err){
+           console.log(err);}     
    }
-   GetFest(year,countryName);
+
+GetFest(year,countryName);
+   
